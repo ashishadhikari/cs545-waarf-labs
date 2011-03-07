2c59636b90ba4f42594075812e45b275374e633a
ashishadhikari@gmail.com <ashishadhikari@gmail.com@53a8ab59-d52f-d77c-2dfe-54fc88496e80>
2011-03-07 00:29:22 +0000

    removed unused import

--- trunk/w2_d4_lab_validation1/src/java/net/mumde/cs545/Tea.java
+++ trunk/w2_d4_lab_validation1/src/java/net/mumde/cs545/Tea.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
